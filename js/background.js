990094676a864b9e819a6d05761ceb181e918202
John Felleman <john.felleman@gsa.gov>
2014-11-26 04:37:13 -0500

    passing data from context menu to popup window

--- js/background.js
+++ js/background.js
@@ -1,10 +1,5 @@
-console.log("gidget background.js");
+console.log('gidget background.js called by ');
 
-var gidgetController = function() { console.log( 'gidgetController function' ) };
-var popup =
-    '<div class="container text-primary">' +
-    'pop' +
-    '</div>';
 chrome.contextMenus.create( {
     type: "normal",
     id: "gidget",
@@ -28,8 +23,19 @@ chrome.contextMenus.create( {
     title: "Share with gateway users",
     parentId: "gidget",
     onclick: function( info, tab ) {
-        alert('nice job clicking in ' + info.pageUrl + '.');
-        window.open( '', 'Share', 'model=yes');
-        // Window.openDialog( '', 'Share', 'modal=yes');
-        },
-    } );
+        var shareDialog = chrome.windows.create(
+            { url: './dialog.html', type: 'popup', width: 521, height: 322 },
+            function(win) {
+                console.log('background page: ' + info.pageUrl );
+                var conn = chrome.tabs.connect( win.tabs[0].id, { name: 'shareInfo' });
+                console.log('background page: connected to ' + win.tabs[0].url);
+                conn.postMessage( { url: info.pageUrl } );
+                chrome.runtime.onConnect.addListener(function(port) {
+                    console.assert(port.name == "shareInfo");
+                    port.onMessage.addListener(function(msg) {
+                        console.log('message received from ' + port.name);
+                    });
+                });
+            });
+    }
+});
--- js/dialog.js
+++ js/dialog.js
@@ -1,101 +1,16 @@
-angular.module('awardOptimizer', []).controller('criteriaController', function($scope) {
-    $scope.toolMode = true;
+angular.module('gidget', []).controller('gidgetController', function($scope) {
 
-    $scope.primaryRequirement = [
-        {
-            label: 'Construction or Architect/Engineering under FAR part 36',
-            value: 1,
-            awardOption: 'na'
-        },
-        {
-            label: 'Product or supply',
-            value: 2,
-            awardOption: 'mas'
-        },
-        {
-            label: 'Non-professional services, ie Service Contract Act',
-            value: 3,
-            awardOption: 'mas'
-        },
-        {
-            label: 'Professional Services',
-            value: 4,
-            awardOption: 'equal'
-        }
-    ];
+    $scope.url = '';
+    $scope.title = '';
+    var shareConnection = chrome.runtime.connect( { name: 'shareInfo' });
 
-
-    $scope.primaryCriterion = $scope.primaryRequirement[1];
-
-    $scope.secReqs = [
-        {
-            checked: false,
-            value: 'NO',
-            label: 'Includes non-commercial services or items',
-            oasis: 'best',
-            mas: 'ng'
-        },
-        {
-            checked: false,
-            value: 'NO',
-            label: 'May include open market items',
-            oasis: 'best',
-            mas: 'possible'
-        },
-        {
-            checked: false,
-            value: 'NO',
-            label: 'Contains or requires a cost reimbursement contract type',
-            oasis: 'best',
-            mas: 'ng'
-        },
-        {
-            checked: true,
-            value: 'NO',
-            label: 'Includes hybrid contract types including cost-type',
-            oasis: 'best',
-            mas: 'ng'
-        },
-        {
-            checked: false,
-            value: 'NO',
-            label: 'Professional Service which includes ancillary goods/services',
-            oasis: 'best',
-            mas: 'possible'
-        },
-        {
-            checked: false,
-            value: 'NO',
-            label: 'Professional services on FFP with no anticipated open market items',
-            oasis: 'possible',
-            mas: 'best'
-        }
-    ];
-
-    $scope.oasisScore = 0;
-    $scope.masScore = 0;
-    $scope.oasisInPlay = true;
-    $scope.masInPlay = true;
-
-    // this function recomputes the score for each solution and enables/disables buttons to avoid
-    // impossible combos
-    $scope.checkboxChanged = function() {
-        console.log('click');
-        $scope.oasisScore = 0;
-        $scope.masScore = 0;
-
-        angular.forEach($scope.secReqs, function(req, key, object) {
-            if (req.value === 'YES' && req.mas === 'best') {
-                $scope.masScore++;
-            }
-            if (req.value === 'YES' && req.oasis === 'best') {
-                $scope.oasisScore++;
-            }
-            if (req.oasis === 'ng' && $scope.oasisInPlay === true) {
-            }
-            if (req.oasis === 'ng' && $scope.oasisInPlay === true) {
-            }
+    chrome.runtime.onConnect.addListener(function(port) {
+        console.assert(port.name == "shareInfo");
+        port.onMessage.addListener(function(msg) {
+            console.log('message "' + msg.url + '" received from ' + port.name);
+            $scope.$apply(function() {
+                $scope.url = msg.url;
+            });
         });
-    };
-
+    });
 });
